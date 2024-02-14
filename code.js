7ac8815a788f96a4dd66a34abec67b09f73a65c2
MelkMan419 <116685383+MelkMan419@users.noreply.github.com>
2024-02-13 19:10:41 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,3 @@
-function mergesort(array) {
-    if (array.length <= 1) {
-        return array;
-    }
-
 const merge = (arr, leftStart, leftEnd, rightStart, rightEnd) => {
     const temp = [];
     let i = leftStart;
@@ -10,42 +5,45 @@ const merge = (arr, leftStart, leftEnd, rightStart, rightEnd) => {
 
     while (i <= leftEnd && j <= rightEnd) {
         if (arr[i] <= arr[j]) {
-            temp.push(arr[i++]);
+            temp.push(arr[i]);
+            i++;
         } else {
-            temp.push(arr[j++]);
+            temp.push(arr[j]);
+            j++;
         }
     }
 
     while (i <= leftEnd) {
-        temp.push(arr[i++]);
+        temp.push(arr[i]);
+        i++;
     }
 
     while (j <= rightEnd) {
-        temp.push(arr[j++]);
+        temp.push(arr[j]);
+        j++;
     }
 
-    for (let k = leftStart; k <= rightEnd; k++) {
-        arr[k] = temp[k - leftStart];
+    for (let k = 0; k < temp.length; k++) {
+        arr[leftStart + k] = temp[k];
     }
 };
 
-
-
-
+const mergesort = (arr) => {
     const mergePass = (arr, size) => {
         for (let i = 0; i < arr.length; i += 2 * size) {
-            const left = arr.slice(i, i + size);
-            const right = arr.slice(i + size, i + 2 * size);
-            const merged = merge(left, right);
-            arr.splice(i, 2 * size, ...merged);
+            const leftStart = i;
+            const leftEnd = Math.min(i + size - 1, arr.length - 1);
+            const rightStart = i + size;
+            const rightEnd = Math.min(i + 2 * size - 1, arr.length - 1);
+            merge(arr, leftStart, leftEnd, rightStart, rightEnd);
         }
     };
 
-    for (let size = 1; size < array.length; size *= 2) {
-        mergePass(array, size);
+    for (let size = 1; size < arr.length; size *= 2) {
+        mergePass(arr, size);
     }
 
-    return array;
-}
+    return arr;
+};
 
 module.exports = mergesort;
